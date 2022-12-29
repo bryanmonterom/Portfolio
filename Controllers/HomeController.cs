c314a05bf1ba0181219d3d8e453953a1bc01fe9a
Bryan <bryanmonterom@gmail.com>
2022-12-29 09:24:00 -0400

    Add projects view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,27 +7,25 @@ namespace Portfolio.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly IRepository repo;
+    private readonly IRepository _repository;
 
-    public HomeController(ILogger<HomeController> logger, IRepository repo)
+    public HomeController(IRepository repo)
     {
-        _logger = logger;
+        _repository = repo; 
     }
 
     public IActionResult Index()
     {
-        var projects = repo.GetProjects();
+        var projects = _repository.GetProjects();
         var model = new PortfolioViewModel(){Projects = projects };
         return View("Index",model);
     }
 
- 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
 
+    public IActionResult Projects() { 
+        var projects = _repository.GetProjects();
+        return View(projects);
+    }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
